0e1774fbb332645f428e8745ceb1d8669f32d588
ivnviv <vanya0774@gmail.com>
2024-02-03 22:37:32 +0300

    git commit --allow-empty -m "Trigger rebuild"

--- src/components/LogoPart.vue
+++ src/components/LogoPart.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="main-picture">
     <div class="bakery-title">
-      <div class="bakery-type">- ТОРТИКОВОЕ ЦАРСТВО -</div>
+      <div class="bakery-type">- ТОРТИКОВОЕ ЦАРСТВО1 -</div>
       <div class="bakery-name">SANYA-BAKERY</div>
     </div>
   </div>
