96af1e438877ff685841c045a0b80b71a115c6c8
ivnviv <vanya0774@gmail.com>
2024-02-19 16:04:51 +0300

    asd

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,15 +1,28 @@
 <template>
-
-
-
+  <div class="main-container">
+    <Header></Header>
+    <LogoPart></LogoPart>
+    <AboutMe></AboutMe>
+    <Galery></Galery>
+    <ChoclateMid></ChoclateMid>
+    <Footer></Footer>
+  </div>
 </template>
 
 <script>
 
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  components: {Footer, Header}
+}
 </script>
 
 <style scoped>
+.main-container {
 
+}
 </style>
 
 
